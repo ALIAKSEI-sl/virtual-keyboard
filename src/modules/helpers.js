3d5782e9c0c4b690adb631a15c95fd426c32a586
Aliaksei <sladkiy.aleksey@gmail.com>
2023-04-29 12:33:57 +0200

    feat: add listeners

---
+++ src/modules/helpers.js
@@ -0,0 +1,50 @@
+export function actionsBackspace(inputField) {
+  const selection = window.getSelection();
+  const startPosition = inputField.selectionStart;
+  const endPosition = inputField.selectionEnd;
+  if (startPosition === endPosition) {
+    if (startPosition !== 0) {
+      inputField.value = inputField.value.slice(0, startPosition - 1)
+      + inputField.value.slice(startPosition);
+      inputField.selectionStart = startPosition - 1;
+      inputField.selectionEnd = startPosition - 1;
+    }
+  } else {
+    inputField.value = inputField.value.slice(0, startPosition)
+      + inputField.value.slice(endPosition);
+    inputField.selectionStart = startPosition;
+    inputField.selectionEnd = startPosition;
+  }
+  selection.removeAllRanges();
+}
+
+export function actionsDelete(inputField) {
+  const selection = window.getSelection();
+  const startPosition = inputField.selectionStart;
+  const endPosition = inputField.selectionEnd;
+  if (startPosition === endPosition) {
+    if (startPosition !== inputField.value.length) {
+      inputField.value = inputField.value.slice(0, startPosition)
+      + inputField.value.slice(startPosition + 1);
+      inputField.selectionStart = startPosition;
+      inputField.selectionEnd = startPosition;
+    }
+  } else {
+    inputField.value = inputField.value.slice(0, startPosition)
+      + inputField.value.slice(endPosition);
+    inputField.selectionStart = startPosition;
+    inputField.selectionEnd = startPosition;
+  }
+  selection.removeAllRanges();
+}
+
+export function actionsInput(inputField, content) {
+  const selection = window.getSelection();
+  const startPosition = inputField.selectionStart;
+  inputField.value = inputField.value.slice(0, startPosition)
+  + content
+  + inputField.value.slice(startPosition);
+  inputField.selectionStart = startPosition + content.length;
+  inputField.selectionEnd = startPosition + content.length;
+  selection.removeAllRanges();
+}
--- src/modules/listener.js
+++ src/modules/listener.js
@@ -1,11 +1,67 @@
-const inputField = document.querySelector('.input-field');
+import { actionsBackspace, actionsDelete, actionsInput } from './helpers.js';
 
-document.addEventListener('keydown', () => {
-  inputField.focus();
-});
+const inputField = document.querySelector('.input-field');
+const wrapper = document.querySelector('.wrapper');
 
 document.addEventListener('keydown', (event) => {
-  if (event.code === 'ShiftLeft' && event.altKey) {
-    // код
+  inputField.focus();
+  const key = document.querySelector(`.${event.code}`);
+  if (key) {
+    key.classList.add('active');
+    if (event.code === 'Tab') {
+      const startPosition = inputField.selectionStart;
+      event.preventDefault();
+      actionsInput(inputField, '  ');
+      inputField.selectionStart = startPosition + 2;
+    }
+  } else {
+    event.preventDefault();
+  }
+
+  if ((event.code === 'ShiftLeft' && event.altKey) || (event.code === 'AltLeft' && event.shiftKey)) {
+    // console.log(event);
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  const key = document.querySelector(`.${event.code}`);
+  if (key) {
+    key.classList.remove('active');
+  }
+});
+
+wrapper.addEventListener('mousedown', (event) => {
+  const key = event.target.parentElement.parentElement;
+  if (key.classList.contains('key')) {
+    const code = key.className.split(' ')[1];
+    key.style.borderRadius = '50px';
+    switch (code) {
+      case ('Backspace'): actionsBackspace(inputField);
+        break;
+      case ('Delete'): actionsDelete(inputField);
+        break;
+      case ('Enter'): inputField.value += '\n';
+        break;
+      case ('ShiftRight'):
+      case ('ControlRight'):
+      case ('AltRight'):
+      case ('AltLeft'):
+      case ('MetaLeft'):
+      case ('ControlLeft'):
+      case ('ShiftLeft'):
+      case ('CapsLock'):
+        break;
+      case ('Tab'): actionsInput(inputField, '  ');
+        break;
+      default:
+        actionsInput(inputField, event.target.textContent);
+    }
+  }
+});
+
+wrapper.addEventListener('mouseup', (event) => {
+  const key = event.target.parentElement.parentElement;
+  if (key.classList.contains('key')) {
+    key.style.borderRadius = '';
   }
 });
